b3de91acf4cdaab5dac0ac75ceb3d3633545fc1f
FlorianAudiger <55442220+FlorianAudiger@users.noreply.github.com>
2021-03-12 09:02:33 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by user190534 on 3/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Projet_IOSWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_IOSWebApp.swift
+//  Shared
+//
+//  Created by user190534 on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Projet_IOSWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
