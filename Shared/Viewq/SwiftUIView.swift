88fb1138e07f55114112913dad29fa9502e1bde7
FlorianAudiger <55442220+FlorianAudiger@users.noreply.github.com>
2021-03-16 21:27:14 +0100

    Summary
    folder

---
+++ Shared/Viewq/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Projet IOSWeb (iOS)
+//
+//  Created by user190534 on 3/16/21.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
