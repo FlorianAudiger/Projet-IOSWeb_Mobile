321aba0a6400789c990d874cb891a6a932cd3be6
FlorianAudiger <55442220+FlorianAudiger@users.noreply.github.com>
2021-03-16 21:31:16 +0100

    Summary
    Base

---
+++ Shared/Models/Jeux.swift
@@ -0,0 +1,8 @@
+//
+//  Jeux.swift
+//  Projet IOSWeb (iOS)
+//
+//  Created by user190534 on 3/16/21.
+//
+
+import Foundation
---
+++ Shared/ViewModel/JeuViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  JeuViewModel.swift
+//  Projet IOSWeb (iOS)
+//
+//  Created by user190534 on 3/16/21.
+//
+
+import Foundation
